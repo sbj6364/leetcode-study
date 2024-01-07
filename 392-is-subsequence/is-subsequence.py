f878e5270c88b9d00d2060bbbe3305ffbee00faf
Josh Seo <sbjun1126@gmail.com>
2024-01-08 05:22:28 +0900

    Time: 39 ms (60.30%) | Memory: 17.5 MB (12.70%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for char in s:
+            if len(t.split(char)) == 1:
+                return False
+            t = t[t.find(char)+1:]
+        return True
