4c29df1001f90f2e08bd339a22ebe345b19ca83f
Josh Seo <sbjun1126@gmail.com>
2024-02-10 21:26:56 +0900

    Time: 399 ms (66.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT machine_id,
+    ROUND(
+        (
+            SELECT AVG(a1.timestamp)
+            FROM Activity a1
+            WHERE a1.activity_type = 'end' and a1.machine_id = a.machine_id
+        ) - (
+            SELECT AVG(a1.timestamp)
+            FROM Activity a1
+            WHERE a1.activity_type = 'start' and a1.machine_id = a.machine_id
+        ), 3
+    )
+    AS processing_time
+FROM Activity a
+GROUP BY a.machine_id
